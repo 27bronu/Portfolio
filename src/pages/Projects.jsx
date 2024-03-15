154a614c9f32bb79496a9704a1ced5782bad78b3
27bronu <brneto14@gmail.com>
2024-03-15 17:11:30 +0000

    feat: added projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useJsonData from '../hooks/useJsonData';
+import Modal from '../components/Modal';
+import extsocial from '../assets/extsocial.png';
 
 export default function Projects() {
   const projectsData = useJsonData();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   if (!projectsData) {
     return <div>Loading...</div>;
   }
 
+  const project = projectsData.projects;
+
   return (
     <div className="home-container">
-      <h2>{projectsData.projects.title}</h2>
+      <div className="projects-container">
+        <div className="project-item">
+          <button onClick={openModal}>{project.title}</button>
+        </div>
+      </div>
+      <Modal
+        showModal={modalOpen}
+        toggleModal={closeModal}
+        content={(
+          <>
+            <img className='project-image' src={extsocial}/>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">{project.view_project}</a>
+          </>
+        )}
+      />
     </div>
   );
 }
